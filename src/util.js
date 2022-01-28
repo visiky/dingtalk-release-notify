c996ccb05a9b21755a87bf3bd5615a5e250ed3f1
visiky <xinming.lxj@antgroup.com>
2022-01-28 23:47:41 +0800

    feat: format releases

--- src/util.js
+++ src/util.js
@@ -3,10 +3,27 @@
  * template('hello, {name}', { name: 'AntV' }); // hello, AntV
  * @param string
  * @param object {data} options
+ * @param string {repository}
  */
-export function template(source, data) {
+export function template(source, data, repository) {
+  function reduceGithubUrl(str) {
+    if (repository) {
+      const urls = str.match(new RegExp(`https://github.com/${repository}/(\\w*)/([\\d\.]*)`, 'g'));
+      if (urls) {
+        urls.forEach(url => {
+          const id = url.match(/.*\/([\d.]*)/)[1];
+          // 把 github 链接，进行缩写. 如: https://github.com/antvis/G2/compare/0.1.2...v0.1.5 -> [0.1.2...v0.1.5](https://github.com/antvis/G2/compare/0.1.2...v0.1.5)
+          // str = str.replace(new RegExp(url, 'g'), id.match(/[^\d]/) ? `[${id}](${url})` : `[#${id}](${url})`);
+          if (id.match(/[^\d]/)) {
+            str = str.replace(new RegExp(url, 'g'), `[${id}](${url})`);
+          }
+        });
+      }
+    }
+    return str;
+  }
   if (!data) {
-    return source;
+    return reduceGithubUrl(source);
   }
   let result = source;
   Object.keys(data).forEach(k => {
@@ -14,5 +31,5 @@ export function template(source, data) {
     result = result.replace(new RegExp(`{\\s*${k}\\s*}`, 'g'), v);
   });
 
-  return result;
+  return reduceGithubUrl(result);
 }
