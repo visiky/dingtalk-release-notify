9bb54c78e74251b6326d33b7f9a90c5c791c2b0b
visiky <xinming.lxj@antgroup.com>
2022-02-07 18:32:10 +0800

    fix: 修复 url 解析问题 & header 展示问题

--- src/util.js
+++ src/util.js
@@ -1,5 +1,5 @@
-const PULL_PATTERN = repo => `https://github.com/${repo}/pull/([\\d]+)`;
-const COMPARE_PATTERN = repo => `https://github.com/${repo}/(compare|commits)/([\\d\.]+)`;
+const PULL_PATTERN = repo => `https://github.com/${repo}/(pull|commit)/([\\d]+)`;
+const COMPARE_PATTERN = repo => `https://github.com/${repo}/(compare|commits)/([\\d\\w\.]+)`;
 
 /**
  * 简单的模板引擎，使用方式如下（空格自动忽略）：
@@ -11,17 +11,23 @@ const COMPARE_PATTERN = repo => `https://github.com/${repo}/(compare|commits)/([
 export function template(source, data, repository) {
   function reduceGithubUrl(str) {
     if (repository) {
-      const urls = str.match(new RegExp(`https://github.com/${repository}/(\\w*)/([\\d\.]*)`, 'g'));
+      const urls = str.match(
+        new RegExp(`[^\\(]https://github.com/${repository}/(\\w*)/([\\d\\w\.]+)`, 'g'),
+      );
       if (urls) {
         urls.forEach(url => {
+          // 去掉前后空白
+          url = url.trim();
           let parsed = url.match(PULL_PATTERN(repository));
-          let hash = parsed && parsed[1];
-          if (!hash) {
-            parsed = utl.match(COMPARE_PATTERN(repository));
-            hash = parsed && parsed[2];
-          }
+          let hash = parsed && parsed[2];
           if (hash) {
-            str = str.replace(new RegExp(url, 'g'), `[#${hash}](${url})`);
+            str = str.replace(new RegExp(`[^\\(]${url}`, 'g'), ` [#${hash}](${url})`);
+          } else {
+            parsed = url.match(COMPARE_PATTERN(repository));
+            hash = parsed && parsed[2];
+            if (hash) {
+              str = str.replace(new RegExp(`[^\\(]${url}`, 'g'), ` [${hash}](${url})`);
+            }
           }
         });
       }
@@ -36,6 +42,8 @@ export function template(source, data, repository) {
     const v = data[k];
     result = result.replace(new RegExp(`{\\s*${k}\\s*}`, 'g'), v);
   });
+
+  result = result.split('\n').join('\n\n');
 
   return reduceGithubUrl(result);
 }
